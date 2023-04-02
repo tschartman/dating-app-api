15112f072d0316cd1dc27b02702b59e2dbd32950
tim <tschartman@yahoo.com>
2023-04-02 14:26:33 -0500

    Fix and push new

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,6 +6,5 @@ const authMiddleware = require('../middlewares/auth');
 router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.post('/verify-token', userController.verifyToken);
-router.post('/verify-token-register', userController.verifyTokenRegister);
 
 module.exports = router;
